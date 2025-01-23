f3216e56b941abbe9af38aa8ce67997b297087f7
ankita-baral <atiknabaral@gmail.com>
2025-01-23 08:00:53 +0545

    Array string and control structure

---
+++ StringContains.java
@@ -0,0 +1,11 @@
+public class StringContains {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+        
+        if (str.contains("Prog")) {
+            System.out.println("Substring found.");
+        } else {
+            System.out.println("Substring not found.");
+        }
+    }
+}
