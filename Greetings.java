0c04f4d6051d2ed285dc950b749627d76a467bb7
ankita-baral <atiknabaral@gmail.com>
2025-01-23 18:46:19 +0545

    class object and overloading

---
+++ Greetings.java
@@ -0,0 +1,11 @@
+public class Greeting {
+    // Method
+    public void sayHello() {
+        System.out.println("Hello, Welcome to java!");
+    }
+
+    public static void main(String[] args) {
+        Greeting greet = new Greeting(); // Object creation
+        greet.sayHello(); // Calling the method
+    }
+}
