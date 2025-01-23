43040888405eb42daa91a0a163ca0b7c8c836b13
ankita-baral <atiknabaral@gmail.com>
2025-01-23 07:58:02 +0545

    datatypes,variables and operators

---
+++ LogicalOperators.java
@@ -0,0 +1,8 @@
+public class LogicalOperators {
+    public static void main(String[] args) {
+        boolean a = true, b = false;
+        System.out.println("a AND b: " + (a && b));  // Logical AND
+        System.out.println("a OR b: " + (a || b));   // Logical OR
+        System.out.println("NOT a: " + (!a));        // Logical NOT
+    }
+}
