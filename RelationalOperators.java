35613ce52e1496fe99db4fddbc00b9a52f4e397d
ankita-baral <atiknabaral@gmail.com>
2025-01-23 07:58:24 +0545

    datatypes,variables and operators

---
+++ RelationalOperators.java
@@ -0,0 +1,9 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int x = 7, y = 10;
+        System.out.println(x + " > " + y + ": " + (x > y));
+        System.out.println(x + " < " + y + ": " + (x < y));
+        System.out.println(x + " == " + y + ": " + (x == y));
+        System.out.println(x + " != " + y + ": " + (x != y));
+    }
+}
